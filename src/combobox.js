d1e2b15278f659ac92a9c35b8de4ff1c2eb725b9
qinhaojie <qinhaojie1992@163.com>
2015-06-16 16:14:25 +0800

    将初始化事件移到新的方法中，添加禁用配置选项，默认不禁用

--- src/combobox.js
+++ src/combobox.js
@@ -9,6 +9,7 @@
         'toggle': '.pn-on',
         'item': '.pn-more p',
         'itemHolder': '.pn-more',
+        'disabled':false,
         'itemTemplate': '<p data-value="{{value}}"><a href="javascript:void(0);">{{text}}</a></p>',
         'warpTemplate': '<div class=\"u-pn fl\">' +
         '<span class=\"pn-on\">' +
@@ -45,6 +46,8 @@
         //初始选中文字
         var initText = $("option:selected", this.el).text();
         this.setText(initText);
+
+        //获取最初元素的选项信息
         $("option", this.el).each(function () {
             initData.push({
                 'value': $(this).val(),
@@ -53,15 +56,37 @@
         });
         this.setItems(initData);
 
+        this.initEvent();
+
+        //设置初始禁用状态
+        this.disable(this.options.disabled);
+
+
+
+    };
+
+
+    /**
+     * 初始化事件
+     */
+    Combobox.prototype.initEvent = function(){
+
         //触发元素上绑定的初始化事件
         this.el.trigger('init.combobox', [this.target, this]);
 
+        //给生成的dom绑定事件
         this.target
             .on('click', this.options.item,
             $.proxy(this.itemClick, this))
             .on('click', this.options.toggle,
             $.proxy(this.toggle, this));
 
+        //绑定元素的change事件，使之改变的时候将下拉框的信息改变
+        this.el.on('change', $.proxy(function (e) {
+            var text = $('option:selected', this.el).text();
+            this.setText(text);
+            this.target.data('value', this.el.val());
+        }, this));
         //点击页面关闭当前处于展开状态的下拉框
         $(document).on('click', function () {
             var combobox = $('body').data('combobox-opened');
@@ -69,8 +94,6 @@
                 combobox.close();
             }
         });
-
-
     };
 
     /**
@@ -184,15 +207,28 @@
         var text = this.el.find('option:selected').html();
         this.setText(text);
         //重新设置下拉框value
-        this.target.data('value',this.el.val());
+        this.target.data('value', this.el.val());
     };
 
     /**
      * 设置下拉框显示文字
      * @param text
      */
-    Combobox.prototype.setText = function(text){
-        $(this.options.textHolder,this.target).text(text);
+    Combobox.prototype.setText = function (text) {
+        $(this.options.textHolder, this.target).text(text);
+    };
+
+    /**
+     * 是否禁用下拉框
+     * @param disabled
+     */
+    Combobox.prototype.disable = function (disabled) {
+        this.disabled = disabled;
+        if (disabled) {
+            this.target.addClass(this.options.disableClass);
+        } else {
+            this.target.removeClass(this.options.disableClass);
+        }
     };
 
     $.fn.combobox = function (options) {
